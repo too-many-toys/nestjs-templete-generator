38b098202a2931ca6c8c34e85ca788fdaa0f0259
mark <34611716+gmae199boy@users.noreply.github.com>
2024-05-16 14:16:10 +0900

    commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from "@nestjs/common";
 import { ServiceModule } from "./service/service.module";
 import { ModuleModule } from "./module/module.module";
 import { ControllerModule } from "./controller/controller.module";
+import { InputModule } from "./input/input.module";
+import { OutputModule } from "./output/output.module";
 
 @Module({
-  imports: [ServiceModule, ModuleModule, ControllerModule],
+  imports: [ServiceModule, ModuleModule, ControllerModule, InputModule, OutputModule],
 })
 export class AppModule {}
